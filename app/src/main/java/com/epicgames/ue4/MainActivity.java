794a06401170b2d9fcfea9f174dfcbf728842edc
Joost Verbraeken <j.verbraeken@hotmail.com>
2017-08-11 11:22:41 +0200

    Refactoring

--- app/src/main/java/com/epicgames/ue4/MainActivity.java
+++ app/src/main/java/com/epicgames/ue4/MainActivity.java
@@ -6,6 +6,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.net.wifi.WifiManager;
+import android.net.wifi.WifiManager.WifiLock;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.support.annotation.NonNull;
@@ -51,39 +52,35 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+@SuppressWarnings({"InstanceVariableOfConcreteClass", "ClassWithoutLogger", "PublicConstructor", "ClassWithTooManyFields", "StaticVariableOfConcreteClass", "PublicMethodWithoutLogging"})
 public final class MainActivity extends WearableActivity implements SensorEventListener, ConnectionCallbacks, OnConnectionFailedListener {
     public static final String TAG = "WearApp";
-    public static final int SENSITIVITY_LIGHT = 11;
-    public static final int SENSITIVITY_MEDIUM = 13;
-    public static final int SENSITIVITY_HARD = 15;
+    public static final int MINIMUM_SHAKING_SENSIVITY = 11;
     private static final String IP_ADDRESS = "192.168.178.29";
     private static final int PORT = 55051;
-    private static final InetAddress INET_ADDRESS;
     private static final long SEND_TIME_THRESHOLD = 1000 / 25; // 20 times per 1000 millisecond (= 20 times per second)
+    private static final float LOW_PASS_FILTER = 0.8f;
     private static final Touch NO_TOUCH = new Touch(-1, -1, (byte) 0);
-    private static final float ALPHA = 0.8f;
-    private static final int DEFAULT_ACCELERATION_THRESHOLD = SENSITIVITY_LIGHT;
+    private static final InetAddress INET_ADDRESS;
+    public static final int MAX_VIBRATION_TIME = 99999;
+    public static final int VIBRATION_DELAY = 650;
+    private static WifiLock wifiLock;
 
     static {
         InetAddress tmp = null;
         try {
             tmp = InetAddress.getByName(IP_ADDRESS);
-        } catch (UnknownHostException e) {
+        } catch (final UnknownHostException e) {
             e.printStackTrace();
         }
         INET_ADDRESS = tmp;
     }
 
     private final SampleQueue queue = new SampleQueue();
-    private final List<Rotation> rotations = Collections.synchronizedList(new ArrayList<Rotation>());
+    private final List<Rotation> rotations = Collections.synchronizedList(new ArrayList<Rotation>(15));
     private final ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
-    /**
-     * When the magnitude of total acceleration exceeds this
-     * value, the phone is accelerating.
-     */
-    private int accelerationThreshold = DEFAULT_ACCELERATION_THRESHOLD;
-    private Acceleration accelerationWithGravity = new Acceleration(0, 0, 0);
-    private Acceleration acceleration = new Acceleration(0, 0, 0);
+    private final Acceleration accelerationWithGravity = new Acceleration(0, 0, 0);
+    private final Acceleration acceleration = new Acceleration(0, 0, 0);
     private Channel channel;
     private GoogleApiClient mGoogleApiClient;
     private Node node;
@@ -94,11 +91,29 @@ public final class MainActivity extends WearableActivity implements SensorEventL
     private Vibrator vibrator;
     private Timer vibrationTimer = new Timer("vibration timer");
     private DatagramSocket datagramSocket;
-    private boolean touchWasOnScreen;
     private boolean newTouchThisSample;
     private Touch touch = NO_TOUCH;
-    private long vibrationStartTime = 0L;
-    private static WifiManager.WifiLock wifiLock;
+    private long vibrationStartTime;
+    private boolean doVibrateWhileShaking;
+
+    public static void keepWiFiOn(final Context context, final boolean on) {
+        if (wifiLock == null) {
+            final WifiManager wm = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+            if (wm != null) {
+                wifiLock = wm.createWifiLock(WifiManager.WIFI_MODE_FULL, TAG);
+                wifiLock.setReferenceCounted(true);
+            }
+        }
+        if (wifiLock != null) {
+            if (on) {
+                wifiLock.acquire();
+                Log.d(TAG, "Acquired WiFi lock");
+            } else if (wifiLock.isHeld()) {
+                wifiLock.release();
+                Log.d(TAG, "Released WiFi lock");
+            }
+        }
+    }
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -144,25 +159,6 @@ public final class MainActivity extends WearableActivity implements SensorEventL
         service.scheduleAtFixedRate(new SendDataRunnable(), 0, SEND_TIME_THRESHOLD, TimeUnit.MILLISECONDS);
     }
 
-    public static void keepWiFiOn(Context context, boolean on) {
-        if (wifiLock == null) {
-            WifiManager wm = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-            if (wm != null) {
-                wifiLock = wm.createWifiLock(WifiManager.WIFI_MODE_FULL, TAG);
-                wifiLock.setReferenceCounted(true);
-            }
-        }
-        if (wifiLock != null) { // May be null if wm is null
-            if (on) {
-                wifiLock.acquire();
-                Log.d(TAG, "Acquired WiFi lock");
-            } else if (wifiLock.isHeld()) {
-                wifiLock.release();
-                Log.d(TAG, "Released WiFi lock");
-            }
-        }
-    }
-
     @Override
     protected void onStart() {
         super.onStart();
@@ -198,26 +194,27 @@ public final class MainActivity extends WearableActivity implements SensorEventL
 
     @Override
     public void onSensorChanged(final SensorEvent sensorEvent) {
-        switch (sensorEvent.sensor.getType()) {
-            case Sensor.TYPE_ROTATION_VECTOR:
-                float[] rotation = new float[3];
-                System.arraycopy(sensorEvent.values, 0, rotation, 0, 3);
-                storeRotation(rotation);
-                break;
-            case Sensor.TYPE_ACCELEROMETER:
-                accelerationWithGravity.x = ALPHA * accelerationWithGravity.x + (1 - ALPHA) * sensorEvent.values[0];
-                accelerationWithGravity.y = ALPHA * accelerationWithGravity.y + (1 - ALPHA) * sensorEvent.values[1];
-                accelerationWithGravity.z = ALPHA * accelerationWithGravity.z + (1 - ALPHA) * sensorEvent.values[2];
-                acceleration.x = sensorEvent.values[0] - accelerationWithGravity.x;
-                acceleration.y = sensorEvent.values[1] - accelerationWithGravity.y;
-                acceleration.z = sensorEvent.values[2] - accelerationWithGravity.z;
-                boolean accelerating = isAccelerating(sensorEvent);
-                long timestamp = sensorEvent.timestamp;
-                queue.add(timestamp, accelerating);
-                if (queue.isShaking()) {
-                    queue.clear();
-                    if (System.currentTimeMillis() > vibrationStartTime + 99999) {
-                        vibrator.vibrate(99999);
+        final int sensorType = sensorEvent.sensor.getType();
+        if (sensorType == Sensor.TYPE_ROTATION_VECTOR) {
+            final float[] rotation = new float[3];
+            System.arraycopy(sensorEvent.values, 0, rotation, 0, 3);
+            storeRotation(rotation);
+        } else if (sensorType == Sensor.TYPE_ACCELEROMETER) {
+            accelerationWithGravity.x = LOW_PASS_FILTER * accelerationWithGravity.x + (1 - LOW_PASS_FILTER) * sensorEvent.values[0];
+            accelerationWithGravity.y = LOW_PASS_FILTER * accelerationWithGravity.y + (1 - LOW_PASS_FILTER) * sensorEvent.values[1];
+            accelerationWithGravity.z = LOW_PASS_FILTER * accelerationWithGravity.z + (1 - LOW_PASS_FILTER) * sensorEvent.values[2];
+            acceleration.x = sensorEvent.values[0] - accelerationWithGravity.x;
+            acceleration.y = sensorEvent.values[1] - accelerationWithGravity.y;
+            acceleration.z = sensorEvent.values[2] - accelerationWithGravity.z;
+            final boolean accelerating = isAccelerating(sensorEvent);
+            final long timestamp = sensorEvent.timestamp;
+            queue.add(timestamp, accelerating);
+            if (queue.isShaking()) {
+                queue.clear();
+                Log.d(TAG, "Shaked!!!");
+                if (doVibrateWhileShaking) {
+                    if (System.currentTimeMillis() > vibrationStartTime + MAX_VIBRATION_TIME) {
+                        vibrator.vibrate(MAX_VIBRATION_TIME);
                         vibrationStartTime = System.currentTimeMillis();
                     }
                     vibrationTimer.cancel();
@@ -229,41 +226,10 @@ public final class MainActivity extends WearableActivity implements SensorEventL
                             vibrator.cancel();
                             vibrationStartTime = 0L;
                         }
-                    }, 650);
-                    Log.d(TAG, "Shaked!!!");
+                    }, VIBRATION_DELAY);
                 }
-                break;
-            default:
-                break;
+            }
         }
-    }
-
-    /**
-     * Returns true if the device is currently accelerating.
-     */
-    private boolean isAccelerating(SensorEvent event) {
-        float ax = event.values[0];
-        float ay = event.values[1];
-        float az = event.values[2];
-
-        // Instead of comparing magnitude to ACCELERATION_THRESHOLD,
-        // compare their squares. This is equivalent and doesn't need the
-        // actual magnitude, which would be computed using (expensive) Math.sqrt().
-        final double magnitudeSquared = ax * ax + ay * ay + az * az;
-        return magnitudeSquared > accelerationThreshold * accelerationThreshold;
-    }
-
-    @SuppressWarnings("NumericCastThatLosesPrecision")
-    private void storeRotation(final float[] values) {
-        final float[] rotMat = new float[9];
-        SensorManager.getRotationMatrixFromVector(rotMat, values);
-        final float[] orientation = new float[3];
-        SensorManager.getOrientation(rotMat, orientation);
-        final float[] rotation = new float[3];
-        rotation[0] = orientation[0] * 180.0f / (float) Math.PI; //Yaw
-        rotation[1] = orientation[1] * 180.0f / (float) Math.PI; //Pitch
-        rotation[2] = orientation[2] * 180.0f / (float) Math.PI; //Roll
-        rotations.add(new Rotation(rotation[0], rotation[1], rotation[2]));
     }
 
     @Override
@@ -288,163 +254,28 @@ public final class MainActivity extends WearableActivity implements SensorEventL
     }
 
     /**
-     * Queue of samples. Keeps a running average.
+     * Returns true if the device is currently accelerating.
      */
-    static class SampleQueue {
+    private static boolean isAccelerating(final SensorEvent event) {
+        final float ax = event.values[0];
+        final float ay = event.values[1];
+        final float az = event.values[2];
 
-        /**
-         * Window size in ns. Used to compute the average.
-         */
-        private static final long MAX_WINDOW_SIZE = 500000000; // 0.5s
-        private static final long MIN_WINDOW_SIZE = MAX_WINDOW_SIZE >> 1; // 0.25s
-
-        /**
-         * Ensure the queue size never falls below this size, even if the device
-         * fails to deliver this many events during the time window. The LG Ally
-         * is one such device.
-         */
-        private static final int MIN_QUEUE_SIZE = 4;
-
-        private final SamplePool pool = new SamplePool();
-
-        private Sample oldest;
-        private Sample newest;
-        private int sampleCount;
-        private int acceleratingCount;
-
-        /**
-         * Adds a sample.
-         *
-         * @param timestamp in nanoseconds of sample
-         */
-        void add(long timestamp, boolean accelerating) {
-            // Purge samples that proceed window.
-            purge(timestamp - MAX_WINDOW_SIZE);
-
-            // Add the sample to the queue.
-            Sample added = pool.acquire();
-            added.timestamp = timestamp;
-            added.accelerating = accelerating;
-            added.next = null;
-            if (newest != null) {
-                newest.next = added;
-            }
-            newest = added;
-            if (oldest == null) {
-                oldest = added;
-            }
-
-            // Update running average.
-            sampleCount++;
-            if (accelerating) {
-                acceleratingCount++;
-            }
-        }
-
-        /**
-         * Removes all samples from this queue.
-         */
-        void clear() {
-            while (oldest != null) {
-                Sample removed = oldest;
-                oldest = removed.next;
-                pool.release(removed);
-            }
-            newest = null;
-            sampleCount = 0;
-            acceleratingCount = 0;
-        }
-
-        /**
-         * Purges samples with timestamps older than cutoff.
-         */
-        void purge(long cutoff) {
-            while (sampleCount >= MIN_QUEUE_SIZE
-                    && oldest != null && cutoff - oldest.timestamp > 0) {
-                // Remove sample.
-                Sample removed = oldest;
-                if (removed.accelerating) {
-                    acceleratingCount--;
-                }
-                sampleCount--;
-
-                oldest = removed.next;
-                if (oldest == null) {
-                    newest = null;
-                }
-                pool.release(removed);
-            }
-        }
-
-        /**
-         * Copies the samples into a list, with the oldest entry at index 0.
-         */
-        List<Sample> asList() {
-            List<Sample> list = new ArrayList<Sample>();
-            Sample s = oldest;
-            while (s != null) {
-                list.add(s);
-                s = s.next;
-            }
-            return list;
-        }
-
-        /**
-         * Returns true if we have enough samples and more than 3/4 of those samples
-         * are accelerating.
-         */
-        final boolean isShaking() {
-            return newest != null
-                    && oldest != null
-                    && newest.timestamp - oldest.timestamp >= MIN_WINDOW_SIZE
-                    && acceleratingCount >= (sampleCount >> 1) + (sampleCount >> 2);
-        }
+        final double magnitudeSquared = ax * ax + ay * ay + az * az;
+        return magnitudeSquared > MINIMUM_SHAKING_SENSIVITY * MINIMUM_SHAKING_SENSIVITY;
     }
 
-    /**
-     * An accelerometer sample.
-     */
-    static class Sample {
-        /**
-         * Time sample was taken.
-         */
-        long timestamp;
-
-        boolean accelerating;
-
-        /**
-         * Next sample in the queue or pool.
-         */
-        Sample next;
-    }
-
-    /**
-     * Pools samples. Avoids garbage collection.
-     */
-    static class SamplePool {
-        private Sample head;
-
-        /**
-         * Acquires a sample from the pool.
-         */
-        Sample acquire() {
-            Sample acquired = head;
-            if (acquired == null) {
-                acquired = new Sample();
-            } else {
-                // Remove instance from pool.
-                head = acquired.next;
-            }
-            return acquired;
-        }
-
-        /**
-         * Returns a sample to the pool.
-         */
-        void release(Sample sample) {
-            sample.next = head;
-            head = sample;
-        }
+    @SuppressWarnings("NumericCastThatLosesPrecision")
+    private void storeRotation(final float[] values) {
+        final float[] rotMat = new float[9];
+        SensorManager.getRotationMatrixFromVector(rotMat, values);
+        final float[] orientation = new float[3];
+        SensorManager.getOrientation(rotMat, orientation);
+        final float[] rotation = new float[3];
+        rotation[0] = orientation[0] * 180.f / (float) Math.PI; //Yaw
+        rotation[1] = orientation[1] * 180.f / (float) Math.PI; //Pitch
+        rotation[2] = orientation[2] * 180.f / (float) Math.PI; //Roll
+        rotations.add(new Rotation(rotation[0], rotation[1], rotation[2]));
     }
 
     private final class ChannelCreateRunnable implements Runnable {
@@ -457,10 +288,10 @@ public final class MainActivity extends WearableActivity implements SensorEventL
                     channel = openChannelResult.getChannel();
                     channel.getOutputStream(mGoogleApiClient).setResultCallback(new ResultCallback<GetOutputStreamResult>() {
                         @Override
-                        public void onResult(@NonNull final GetOutputStreamResult getOutputStreamResult) {
+                        public void onResult(@NonNull final GetOutputStreamResult outputStreamResult) {
                             Log.d(TAG, "Creating DataOutputStream");
 
-                            channelOutputStream = new DataOutputStream(getOutputStreamResult.getOutputStream());
+                            channelOutputStream = new DataOutputStream(outputStreamResult.getOutputStream());
                         }
                     });
                 }
@@ -480,11 +311,7 @@ public final class MainActivity extends WearableActivity implements SensorEventL
 
                 synchronized (MainActivity.this) {
                     if (newTouchThisSample) {
-                        touchWasOnScreen = true;
                         newTouchThisSample = false;
-                    }
-                    if (touch.equals(NO_TOUCH)) {
-                        touchWasOnScreen = false;
                     }
 
                     cachedThreadPool.execute(new UDPRunnable(avgRotation, acceleration, touch));
@@ -551,10 +378,10 @@ public final class MainActivity extends WearableActivity implements SensorEventL
                 }
                 dataOutputStream.writeLong(touch.timestamp);
 
-                byte[] byteArray = byteArrayOutputStream.toByteArray();
-                byte[] bytes = ByteBuffer.allocate(byteArray.length).put(byteArray).array();
+                final byte[] byteArray = byteArrayOutputStream.toByteArray();
+                final byte[] bytes = ByteBuffer.allocate(byteArray.length).put(byteArray).array();
                 datagramSocket.send(new DatagramPacket(bytes, byteArray.length, INET_ADDRESS, PORT));
-            } catch (IOException e) {
+            } catch (final IOException e) {
                 e.printStackTrace();
             }
         }
@@ -565,7 +392,7 @@ public final class MainActivity extends WearableActivity implements SensorEventL
         private final Acceleration acceleration;
         private final Touch touch;
 
-        ChannelRunnable(Rotation rotation, Acceleration acceleration, Touch touch) {
+        ChannelRunnable(final Rotation rotation, final Acceleration acceleration, final Touch touch) {
             this.rotation = rotation;
             this.acceleration = acceleration;
             this.touch = touch;
---
+++ app/src/main/java/com/epicgames/ue4/SampleQueue.java
@@ -0,0 +1,95 @@
+package com.epicgames.ue4;
+
+import android.support.annotation.Nullable;
+
+class SampleQueue {
+    private static final long MAX_WINDOW_SIZE = 500000000;
+    private static final long MIN_WINDOW_SIZE = MAX_WINDOW_SIZE >> 1;
+    private static final int MIN_QUEUE_SIZE = 4;
+    private final SamplePool pool = new SamplePool();
+    private Sample oldest;
+    @Nullable
+    private Sample newest;
+    private int sampleCount;
+    private int acceleratingCount;
+
+    @SuppressWarnings("NonBooleanMethodNameMayNotStartWithQuestion")
+    final void add(final long timestamp, final boolean accelerating) {
+        purge(timestamp - MAX_WINDOW_SIZE);
+
+        final Sample added = pool.acquire();
+        added.timestamp = timestamp;
+        added.accelerating = accelerating;
+        added.next = null;
+        if (newest != null) {
+            newest.next = added;
+        }
+        newest = added;
+        if (oldest == null) {
+            oldest = added;
+        }
+        sampleCount++;
+        if (accelerating) {
+            acceleratingCount++;
+        }
+    }
+
+    final void clear() {
+        while (oldest != null) {
+            final Sample removed = oldest;
+            oldest = removed.next;
+            pool.release(removed);
+        }
+        newest = null;
+        sampleCount = 0;
+        acceleratingCount = 0;
+    }
+
+    final void purge(final long cutoff) {
+        while (sampleCount >= MIN_QUEUE_SIZE
+                && oldest != null && cutoff - oldest.timestamp > 0) {
+            final Sample removed = oldest;
+            if (removed.accelerating) {
+                acceleratingCount--;
+            }
+            sampleCount--;
+
+            oldest = removed.next;
+            if (oldest == null) {
+                newest = null;
+            }
+            pool.release(removed);
+        }
+    }
+
+    final boolean isShaking() {
+        return newest != null
+                && oldest != null
+                && newest.timestamp - oldest.timestamp >= MIN_WINDOW_SIZE
+                && acceleratingCount >= (sampleCount >> 1) + (sampleCount >> 2);
+    }
+    private static class Sample {
+        long timestamp;
+        boolean accelerating;
+        @Nullable
+        Sample next;
+    }
+
+    private static class SamplePool {
+        private Sample head;
+        private Sample acquire() {
+            Sample acquired = head;
+            if (acquired == null) {
+                acquired = new Sample();
+            } else {
+                head = acquired.next;
+            }
+            return acquired;
+        }
+
+        final void release(final Sample sample) {
+            sample.next = head;
+            head = sample;
+        }
+    }
+}
